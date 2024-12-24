280ab7a47076d76b07db944a59e82ad4f2a5075e
Luis <211g0373@rcarbonifera.tecnm.mx>
2024-12-24 12:24:29 -0600

    wq

--- BarbieMovies/Program.cs
+++ BarbieMovies/Program.cs
@@ -1,6 +1,27 @@
-var builder = WebApplication.CreateBuilder(args);
-var app = builder.Build();
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
 
-app.MapGet("/", () => "Hello World!");
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddMvc();
+builder.Services.AddDbContext<BarbieMovies.Models.BarbiemoviesContext>(
+   optionsBuilder => optionsBuilder.UseMySql("server=localhost;user=root;database=barbiemovies;password=root", Microsoft.EntityFrameworkCore.ServerVersion.Parse("8.0.40-mysql"))
+    );
+
+
+
+
+
+
+var app = builder.Build();
+app.UseStaticFiles();
+app.MapAreaControllerRoute(
+    name: "areas",
+    areaName: "Admin",
+    pattern: "{area:exists}/{controller=Home}/{action=Index}/{id?}"
+    );
+app.MapDefaultControllerRoute();
+
+
 
 app.Run();
