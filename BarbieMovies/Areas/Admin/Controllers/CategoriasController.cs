2e0b31d62a89220ceb982b769a920d4508d5f098
Luis <211g0373@rcarbonifera.tecnm.mx>
2024-12-24 15:41:20 -0600

    Crud terminado

--- BarbieMovies/Areas/Admin/Controllers/CategoriasController.cs
+++ BarbieMovies/Areas/Admin/Controllers/CategoriasController.cs
@@ -17,47 +17,94 @@ namespace BarbieMovies.Areas.Admin.Controllers
 
 
 
-
+        [Route("/admin/Categorias/index")]
+        [Route("/admin/Categorias")]
         public IActionResult Index()
         {
-            return View();
+            var cat = Context.Categories.OrderBy(x => x.Name);
+            return View(cat);
         }
-
+        [Route("/admin/Categorias/Agregar")]
         [HttpGet]
         public IActionResult Agregar()
         {
-
-            return View();
+            Categories c=new Categories();
+            return View(c);
         }
 
         [HttpPost]
         public IActionResult Agregar(Categories c)
         {
-            return View();
+            if (string.IsNullOrWhiteSpace(c.Name))
+            {
+                ModelState.AddModelError("", "");
+            }else if (Context.Categories.Any(x => x.Name == c.Name))
+            {
+                ModelState.AddModelError("", "");
+            }
+
+            if (ModelState.IsValid)
+            {
+                Context.Add(c);
+                Context.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(c);
         }
+        [Route("/admin/Categorias/Editar/{id}")]
 
         [HttpGet]
         public IActionResult Editar(int id)
         {
-            return View();
+            var c = Context.Categories.FirstOrDefault(x => x.Id == id);
+            return View(c);
 
         }
 
         [HttpPost]
         public IActionResult Editar(Categories c)
         {
+            if (string.IsNullOrWhiteSpace(c.Name))
+            {
+                ModelState.AddModelError("", "");
+            }
+            else if (Context.Categories.Any(x => x.Name == c.Name && x.Id!=c.Id))
+            {
+                ModelState.AddModelError("", "");
+            }
+            if (ModelState.IsValid)
+            {
+                var cat = Context.Categories.FirstOrDefault(x => x.Id == c.Id);
+                if (cat != null) { 
+                cat.Name = c.Name;
+                    Context.SaveChanges();
+                    return RedirectToAction("Index");
+                }
+
+            }
             return View();
         }
+        [Route("/admin/Categorias/Eliminar/{id}")]
 
         [HttpGet]
         public IActionResult Eliminar(int id)
         {
-            return View();
+
+            var c = Context.Categories.FirstOrDefault(x => x.Id == id);
+            return View(c);
         }
 
         [HttpPost]
         public IActionResult Eliminar(Categories c)
         {
+            var cat = Context.Categories.FirstOrDefault(x => x.Id == c.Id);
+            if (cat != null)
+            {
+                Context.Remove(cat);
+                Context.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            
             return View();
         }
     }
