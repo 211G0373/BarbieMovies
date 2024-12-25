294f4334adadf405a7ec287b5f71990a400d5749
Luis <211g0373@rcarbonifera.tecnm.mx>
2024-12-25 15:29:28 -0600

    e

--- BarbieMovies/Areas/Admin/Controllers/PeliculasController.cs
+++ BarbieMovies/Areas/Admin/Controllers/PeliculasController.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using BarbieMovies.Areas.Admin.Models;
+using BarbieMovies.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace BarbieMovies.Areas.Admin.Controllers
 {
@@ -6,9 +8,55 @@ namespace BarbieMovies.Areas.Admin.Controllers
 
     public class PeliculasController : Controller
     {
+        public BarbiemoviesContext Context { get; }
+
+        public PeliculasController(BarbiemoviesContext context)
+        {
+            Context = context;
+        }
+        [Route("/admin/Categorias/index")]
+        [Route("/admin/Categorias")]
         public IActionResult Index()
         {
             return View();
         }
+        [Route("/admin/Categorias/Agregar")]
+        [HttpGet]
+        public IActionResult Agregar()
+        {
+            return View();
+        }
+        [HttpGet]
+        public IActionResult Agregar(AgregarViewModel p)
+        {
+            return View(p);
+        }
+
+        [Route("/admin/Categorias/Editar/{id}")]
+        [HttpGet]
+        public IActionResult Editar(int id)
+        {
+            return View();
+        }
+        [HttpGet]
+        public IActionResult Editar(AgregarViewModel p)
+        {
+            return View(p);
+        }
+
+
+        [Route("/admin/Categorias/Eliminar/{id}")]
+        [HttpGet]
+        public IActionResult Eliminar(int id)
+        {
+            return View();
+        }
+        [HttpGet]
+        public IActionResult Eliminar(AgregarViewModel p)
+        {
+            return View(p);
+        }
+
+
     }
 }
